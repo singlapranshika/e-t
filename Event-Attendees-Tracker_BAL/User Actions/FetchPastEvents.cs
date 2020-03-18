7de41c4ea71901831700e102eac7b40474d9d2ce
GnanaVivek Ponnuru <Gnanavivek_Ponnuru@epam.com>
2020-03-18 15:16:04 +0530

    Added Reports page

---
+++ Event-Attendees-Tracker_BAL/User Actions/FetchPastEvents.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Event_Attendees_Tracker_DAL.DBQueries;
+using Event_Attendees_Tracker_DAL.Models;
+
+using Event_Attendees_Tracker_CustomResponseModel;
+namespace Event_Attendees_Tracker_BAL.User_Actions
+{
+   public class FetchPastEvents
+    {
+      
+        public static List<PastEventResponseModel> fetchPastEventAttendeesData(int userId)
+        {
+            try
+            {
+                var pastEventResponseDetails = PastEvent.fetchPastEventAttendeesData(userId);
+                if (pastEventResponseDetails == null)
+                {
+                    return null;
+                }
+                return pastEventResponseDetails;
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+    }
+}
